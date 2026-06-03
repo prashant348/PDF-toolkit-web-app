7627cb74944acbb7047f957aff3e2d5f89d11be6
codingweapon <prashantmistri348@gmail.com>
2026-06-03 17:24:36 +0530

    server: refactor requirements.txt and redis.py

--- server/app/core/redis.py
+++ server/app/core/redis.py
@@ -1,11 +1,28 @@
 from redis import Redis, exceptions
+from upstash_redis import Redis as UpstashRedis
+import os
 
-redis_client = Redis(
-    host="localhost",
-    port=6379,
-    db=0,
-    decode_responses=True
-)
+env = os.getenv("ENV")
+
+if env == "developement":
+    host = os.getenv("REDIS_HOST")
+    port = os.getenv("REDIS_PORT")
+
+    redis_client = Redis(
+        host=host,
+        port=port,
+        db=0,
+        decode_responses=True
+    )
+else:
+    redis_url = os.getenv("UPSTASH_REDIS_REST_URL")
+    redis_token = os.getenv("UPSTASH_REDIS_REST_TOKEN")
+
+    redis_client = UpstashRedis(
+        url=redis_url,
+        token=redis_token
+        )
+
 
 def get_redis_client():
     try:
